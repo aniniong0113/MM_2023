f79790d327ed1268ed1671fad5fbb36f491073f8
aniniong0113 <89166298+aniniong0113@users.noreply.github.com>
2023-02-24 12:14:02 +1300

    Add files via upload

---
+++ yesNoChecker.py
@@ -0,0 +1,11 @@
+# functions go here
+
+# main routine goes here
+want_instructions = input("Do you want the instructions?")
+
+if want_instructions == "yes":
+    print("Instructions go here")
+elif want_instructions == "no":
+    pass
+else:
+    print("try again, please answer yes / no")
